fd83f90235ace92988760eaf82c105bb67da8638
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-12-02 15:20:53 +0000

    Fix gluLookAt

--- hw1-windows/hw1-windows/Transform.cpp
+++ hw1-windows/hw1-windows/Transform.cpp
@@ -37,8 +37,11 @@ mat4 Transform::lookAt(vec3 eye, vec3 up) {
   
 	vec3 center = vec3(0, 0, 0);
 
-	vec3 w = glm::normalize(center - eye);
-	vec3 u = glm::cross(up, w);
+	vec3 a = eye - center;
+	vec3 b = up;
+
+	vec3 w = glm::normalize(a);
+	vec3 u = glm::cross(b, w);
 	u = glm::normalize(u);
 	vec3 v = glm::cross(w, u);
 
@@ -47,21 +50,21 @@ mat4 Transform::lookAt(vec3 eye, vec3 up) {
 	m[0][0] = u.x;
 	m[0][1] = u.y;
 	m[0][2] = u.z;
-	m[0][3] = -1.0f * glm::dot(u, eye);
+	m[0][3] = -glm::dot(u, eye);
 
 	m[1][0] = v.x;
 	m[1][1] = v.y;
 	m[1][2] = v.z;
-	m[1][3] = -1.0f * glm::dot(v, eye);
+	m[1][3] = -glm::dot(v, eye);
 
 	m[2][0] = w.x;
 	m[2][1] = w.y;
 	m[2][2] = w.z;
-	m[2][3] = -1.0f * glm::dot(w, eye);
+	m[2][3] = -glm::dot(w, eye);
 
 	m[3][0] = 0;
 	m[3][1] = 0;
-	m[3][2] = 1;
+	m[3][2] = 0;
 	m[3][3] = 1;
 
 	return glm::transpose(m);
