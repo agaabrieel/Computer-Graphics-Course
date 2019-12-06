05dc228cc448c3b6c2486fab1fe0a7c49aef28e6
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-12-06 10:07:22 +0000

    Tidy up readfile.cpp

--- hw2-windows/hw2-windows/readfile.cpp
+++ hw2-windows/hw2-windows/readfile.cpp
@@ -107,13 +107,7 @@ void readfile(const char* filename)
 							for (int i = 4; i < 8; i++) {
 								lightcolor[numused * 4 + (i - 4)] = values[i];
 							}
-
-							// TODO delete these comments once satisfied it works.
-                            // YOUR CODE FOR HW 2 HERE. 
-                            // Note that values[0...7] shows the read in values 
-                            // Make use of lightposn[] and lightcolor[] arrays in variables.h
-                            // Those arrays can then be used in display too.  
-
+							
                             ++numused; 
                         }
                     }
@@ -180,14 +174,6 @@ void readfile(const char* filename)
 						}
 
 						fovy = values[9];
-
-						// TODO delete these comments once satisfied they work.
-                        // YOUR CODE FOR HW 2 HERE
-                        // Use all of values[0...9]
-                        // You may need to use the upvector fn in Transform.cpp
-                        // to set up correctly. 
-                        // Set eyeinit upinit center fovy in variables.h 
-
                     }
                 }
 
@@ -234,13 +220,6 @@ void readfile(const char* filename)
 
 						mat4 translationMatrix = Transform::translate(values[0], values[1], values[2]);
 						rightmultiply(translationMatrix, transfstack);
-
-						// TODO delete these comments once satisfied it works.
-                        // YOUR CODE FOR HW 2 HERE.  
-                        // Think about how the transformation stack is affected
-                        // You might want to use helper functions on top of file. 
-                        // Also keep in mind what order your matrix is!
-
                     }
                 }
                 else if (cmd == "scale") {
@@ -249,12 +228,6 @@ void readfile(const char* filename)
 
 						mat4 scaleMatrix = Transform::scale(values[0], values[1], values[2]);
 						rightmultiply(scaleMatrix, transfstack);
-
-                        // YOUR CODE FOR HW 2 HERE.  
-                        // Think about how the transformation stack is affected
-                        // You might want to use helper functions on top of file.  
-                        // Also keep in mind what order your matrix is!
-
                     }
                 }
                 else if (cmd == "rotate") {
@@ -265,15 +238,6 @@ void readfile(const char* filename)
 							Transform::rotate(values[3], vec3(values[0], values[1], values[2])));
 
 						rightmultiply(rotateMatrix, transfstack);
-
-						// TODO delete these comments once satisfied the code works.
-                        // YOUR CODE FOR HW 2 HERE. 
-                        // values[0..2] are the axis, values[3] is the angle.  
-                        // You may want to normalize the axis (or in Transform::rotate)
-                        // See how the stack is affected, as above.  
-                        // Note that rotate returns a mat3. 
-                        // Also keep in mind what order your matrix is!
-
                     }
                 }
 
