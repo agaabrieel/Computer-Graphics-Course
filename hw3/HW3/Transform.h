ef5f5f983f709d2abe6a9746cff65591f944fc01
Josh Taylor <35666594+xxylem@users.noreply.github.com>
2019-12-07 14:50:09 +0000

    Move Transforms to MatrixStack
    Rename MatrixStack to TransformStack.
    
    You can no longer directly right multiply the top of the stack using a matrix. Instead, you must specify a transformation to be applied, either scale, rotate or translate, and it is done internally.
    
    This hides the matrix operations from the user.

--- hw3/HW3/Transform.h
+++ hw3/HW3/Transform.h
@@ -5,11 +5,3 @@
 // namespace Transform
 
 
-// Scale(sx, sy, sz)
-	// Returns a matrix that will scale the view 
-
-// Translate(tx, ty, tz)
-	// Returns a matrix that will translate the view
-
-// Rotate(rx, ry, rz)
-	// Returns a matrix that will rotate the view
---
+++ hw3/HW3/TransformStack.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <stack>
+#ifndef GLM_FORCE_RADIANS
+#define GLM_FORCE_RADIANS
+#endif
+#include <glm/glm.hpp>
+#include "Direction.h"
+
+/*
+A Transform Stack has:
+
+a stack of matrices
+
+A TransformStack can:
+
+give you the top of the stack (a matrix)
+duplicate the top of the stack push(top) to set up new transforms
+pop the top of the stack to get rid of the last group of transforms
+
+rotate the top matrix
+translate the top matrix
+scale the top matrix
+
+// Scale(sx, sy, sz)
+	// Returns a matrix that will scale the view
+
+// Translate(tx, ty, tz)
+	// Returns a matrix that will translate the view
+
+// Rotate(rx, ry, rz)
+	// Returns a matrix that will rotate the view
+
+	// Transformations are applied to the top matrix in the stack.
+
+*/
+
+class TransformStack
+{
+	public:
+		TransformStack(); // Do we start with identity?
+
+		glm::mat4& top();
+
+		void pushTransform();
+		void popTransform();
+
+		void scale(const float& sx, const float& sy, const float& sz);
+		void translate(const float& tx, const float& ty, const float& tz);
+		void rotate(const float degrees, const Direction& axis);
+
+	private:
+		const std::stack<glm::mat4> _transformationStack;
+};
+
